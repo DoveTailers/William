26dbeedfa78ef62d4ec89bd28cb5b5b5151cdf03
William Chen (Yi Tian) <willch.chen@mail.utoronto.ca>
2016-02-06 23:41:56 -0500

    basic wave structure

---
+++ Invasion 1945 Prototype/Assets/Scripts/GameController_Wave.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController_Wave : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector2 spawnValues;
+	public Vector2 spawnValues2;
+	public float waveWait1, waveWait2;
+
+	void Start () {
+		StartCoroutine (SpawnWaves (spawnValues, waveWait1));
+		StartCoroutine (SpawnWaves (spawnValues2, waveWait2));
+	}
+
+	IEnumerator SpawnWaves (Vector2 sv, float wWait) {
+		yield return new WaitForSeconds (wWait);
+		Vector2 spawnPosition = new Vector2 (sv.x, sv.y);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+
+}
---
+++ Invasion 1945 Prototype/Assets/Scripts/PlayerController_Wave.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController_Wave : MonoBehaviour {
+	public float speed;
+	Rigidbody2D rb;
+	public Boundary boundary;
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	
+	private float nextFire;
+	
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity = transform.up.normalized*speed;
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical); 
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector2 (
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),  
+			Mathf.Clamp (rb.position.y, boundary.yMin, boundary.yMax)
+		);
+	}
+}
